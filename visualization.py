1d45a2d76d703e3a6ea91dd7d2f0cc041c008198
Vaclav Hodina <vhodina@redhat.com>
2016-01-05 15:51:41 +0100

    Modified createGraph function so that it excludes cdroms

--- visualization.py
+++ visualization.py
@@ -11,12 +11,22 @@ class Visualization(object):
 
 		node_list = []
 		edge_list = []
+		#blacklist of devices that should not be displayed in a graph, i.e. cdrom
+		blacklist = ["cdrom"]
 		for n in blvt.devices:
-			node_to_be_added = node.Node(n.name,n.type,n.size)
-			node_list.append(node_to_be_added)	
+			if not n.type in blacklist:
+				node_to_be_added = node.Node(n.name,n.type,n.size)
+				node_list.append(node_to_be_added)	
+				print "Adding " + n.name + " " + n.type# + " " +  n.size
+			else:
+				break
 			if n.parents:
-				edge_to_be_added = edge.Edge(n.parents[0].name,n.name)
-				edge_list.append(edge_to_be_added)
+				print " " + n.parents[0].name
+				if n.parents[0].type == "luks/dm-crypt":
+					edge_to_be_added = edge.Edge(n.parents[0].parents[0].name,n.name)
+				else:
+					edge_to_be_added = edge.Edge(n.parents[0].name,n.name)
+					edge_list.append(edge_to_be_added)
 
 		graph = gv.digraph("Graph")
 		for i in node_list:

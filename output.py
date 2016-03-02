2ab164e0db6ec286b07955b08ab237ba629515c9
Vaclav Hodina <vhodina@redhat.com>
2016-03-02 14:00:00 +0100

    Added back node attribudes in python 3

--- output.py
+++ output.py
@@ -1,8 +1,6 @@
 import graphviz 
 
 class Output:
-#TODO get attributes back in python3
-
 	def __init__(self, node_list, edge_list):
 		self.graph=self.createGvGraph(node_list, edge_list)
 
@@ -11,12 +9,10 @@ class Output:
 		svg_file.write(self.graph.pipe(format="svg").decode(encoding="UTF-8"))
 		svg_file.close()
 	
-	#def createGtk(self,graph_name):
-
 	def createGvGraph(self,node_list, edge_list):
 		graph = graphviz.Digraph("degraph")
 		for i in node_list:
-			new_node = graph.node(str(i.getName()))#,i.getGvAttributes().items())
+			new_node = graph.node(str(i.getName()),i.getGvAttributes()["label"],i.getGvAttributes())
 		for i in edge_list:
 			graph.edge(str(i.getFrom()),str(i.getTo()))	
 		return graph

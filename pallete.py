15dab5c26b995894277135a65ffc91b5e91b7f9d
Vaclav Hodina <vhodina@redhat.com>
2016-04-10 21:55:53 +0200

    added a xml pallete and its loader class

--- pallete.py
+++ pallete.py
@@ -3,7 +3,7 @@ import xml.etree.ElementTree as ET
 class Pallete:
     def __init__(self):
         tree = ET.parse("pallete.xml")
-        root = tree.getroot()
+        self.root = tree.getroot()
         self.primary = self.fill_colorset("primary")
         self.secondary_first = self.fill_colorset("secondary-1")
         self.secondary_second = self.fill_colorset("secondary-2")
@@ -11,13 +11,14 @@ class Pallete:
 
     def fill_colorset(self, id):
         colorset = {}
-        colorset_element = root.find("colorset{id='" + id + "'")
-        for color in colorset_element.iter():
+        colorset_element = self.root.find("./colorset[@id='" + id + "']")
+        for color in colorset_element:
             colorset[color.get("nr")] = "#" + color.get("rgb")
         return colorset
 
 p = Pallete()
 print(p.primary)
+print(p.primary["1"])
 print(p.secondary_first)
 print(p.secondary_second)
 print(p.complement)

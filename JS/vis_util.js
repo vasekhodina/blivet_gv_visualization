3232705bb7c522934fa86ae6a84efc5862e82340
Vaclav Hodina <vhodina@redhat.com>
2016-04-15 17:03:51 +0200

    Created functions for zoom in and out, useful for solving issue #14

--- JS/vis_util.js
+++ JS/vis_util.js
@@ -5,30 +5,36 @@ function zoomNode(node){
     }
     text_elements=node.getElementsByTagName("text")
     if (node.getAttribute("zoomed") == "0"){
-        node.parentNode.appendChild(node);
-        x=text_elements[0].getAttribute("x")
-        y=text_elements[0].getAttribute("y")
-        node.setAttribute("transform","translate(" + -x + "," + -y + ") scale(2)");
-        node.setAttribute("zoomed","1");
-
-        for (i=0; i<3;i++){
-            text_elements[i].setAttribute("font-size","14.00")
-            text_elements[i].removeAttribute("transform")
-        }
-        for (i=3; i<text_elements.length;i++){
-            text_elements[i].setAttribute("visibility","visible")
+        zoom_in(node)
         }
     } else {
-        node.setAttribute("transform","scale(1)");
-        node.setAttribute("zoomed","0");
+        zoom_out(node)
+    }
+}
+function zoom_in(node){
+    node.parentNode.appendChild(node);
+    x=text_elements[0].getAttribute("x")
+    y=text_elements[0].getAttribute("y")
+    node.setAttribute("transform","translate(" + -x + "," + -y + ") scale(2)");
+    node.setAttribute("zoomed","1");
 
-        for (i=0; i<3;i++){
-            var transform_x = i*10;
-            text_elements[i].setAttribute("font-size","18.00")
-            text_elements[i].setAttribute("transform","translate(0 "+transform_x+")")
-        }
-        for (i=3; i<text_elements.length;i++){
-            text_elements[i].setAttribute("visibility","hidden")
-        }
+    for (i=0; i<3;i++){
+        text_elements[i].setAttribute("font-size","14.00")
+        text_elements[i].removeAttribute("transform")
+    }
+    for (i=3; i<text_elements.length;i++){
+        text_elements[i].setAttribute("visibility","visible")
+}
+function zoom_out(node){
+    node.setAttribute("transform","scale(1)");
+    node.setAttribute("zoomed","0");
+
+    for (i=0; i<3;i++){
+        var transform_x = i*10;
+        text_elements[i].setAttribute("font-size","18.00")
+        text_elements[i].setAttribute("transform","translate(0 "+transform_x+")")
+    }
+    for (i=3; i<text_elements.length;i++){
+        text_elements[i].setAttribute("visibility","hidden")
     }
 }

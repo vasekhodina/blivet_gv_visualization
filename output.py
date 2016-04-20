3fd101426d7485b68fa18832ea934086a471bfb3
Vaclav Hodina <vhodina@redhat.com>
2016-04-20 19:48:30 +0200

    Added a line so that edge attributes are added

--- output.py
+++ output.py
@@ -18,7 +18,7 @@ class Output:
             new_node = graph.node(str(i.getName()), i.getGvAttributes()["label"],
                                   i.getGvAttributes())
         for i in edge_list:
-            graph.edge(str(i.getFrom()), str(i.getTo()))
+            graph.edge(str(i.getFrom()), str(i.getTo()), attributes=i.gv_attributes)
         return graph
 
     def insert_JS_to_graph(self, graph_string):

2ae3b22a4a7e145c659a6c970b8f7ca7ff5ebd43
Vaclav Hodina <vhodina@redhat.com>
2016-05-01 19:38:06 +0200

    Added sphinx docstrings to actionsProcessor

--- actionsProcessor.py
+++ actionsProcessor.py
@@ -3,6 +3,7 @@ import node
 import gvInput
 
 class ActionsProcessor():
+    """ Class containing neccessary methods for putting actions scheduled by blivet into graph"""
     def __init__(self, actions_list, node_list, edge_list):
         self.actions = actions_list
         self.node_list = node_list
@@ -11,6 +12,7 @@ class ActionsProcessor():
         self.gv_input = gvInput.GvInput(node_list, edge_list)
 
     def processActions(self):
+        """ Main method for processing actions """
         print("Actions:")
         for action in self.actions:
             found_node = self.find_node(action.device.name)
@@ -20,6 +22,9 @@ class ActionsProcessor():
             self.process_action(action, found_node)
 
     def process_action(self, action, node):
+        """ Helper function for processing actions, finds out what does each action do and sets the appropriate attributed of the node
+        :param obj action The action to be processed.
+        :param obj node The node to be changed."""
         if action.isDestroy or isRemove:
             node.addAttribute("action", "Delete")
             node.change_color(self.pallete.complement["2"])
@@ -34,6 +39,8 @@ class ActionsProcessor():
             node.change_color(self.pallete.secondary_first["2"])
 
     def find_node(self, dev_name):
+        """ Helper function that searches node_list for a node using it's name
+            :param str dev_name The name of node / device that should be looked up.
         print("In find_node")
         for found_node in self.node_list:
             print("Checking: " + found_node.getName() +  " and " + dev_name)

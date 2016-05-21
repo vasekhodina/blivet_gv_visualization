a7e9b962fbfc6f7c50cae53cde3bdb2fc0f3475d
Vaclav Hodina <vhodina@redhat.com>
2016-05-21 16:35:20 +0200

    Finalized the look of the application

--- gui.py
+++ gui.py
@@ -10,44 +10,45 @@ gi.require_version('WebKit', '3.0')
 from gi.repository import Gtk, Rsvg, GdkPixbuf, WebKit
 
 
-# TODO Clean up this flippin mess
 class Gui(Gtk.Window):
     def __init__(self, dirname, graph_name):
+        self.HEIGHT = 600
+        self.WIDTH = 800
         self.dirname = dirname
         self.graph_name = graph_name
+        # Init window
         Gtk.Window.__init__(self, title="Disk data visualization")
+        self.set_default_size(self.WIDTH, self.HEIGHT)
+        # Header Bar
+        self.hb = Gtk.HeaderBar()
+        self.hb.set_show_close_button(True)
+        self.hb.props.title = "Disk data visualization"
+        self.set_titlebar(self.hb)
+        # Scroll
+        self.scroll = Gtk.ScrolledWindow(None, None)
+        # Webview
         self.webview = WebKit.WebView()
-        settings = self.webview.get_settings()
-        self.grid = Gtk.Grid()
-        self.add(self.grid)
-        self.set_default_size(800, 600)
-        self.layout = Gtk.Layout()
-        self.layout.put(self.webview, 50, 50)
-        self.layout.set_vexpand(True)
-        self.layout.set_hexpand(True)
-
-        self.button = Gtk.Button(label="Do the magic!")
-        self.layout.put(self.button, 6, 6)
+        self.webview.set_size_request(self.WIDTH - 50, self.HEIGHT - 80)
+        # Button
+        self.button = Gtk.Button(label="Help")
         self.button.connect("clicked", self.on_button_clicked)
-        self.grid.attach(self.layout, 0, 0, 1, 1)
-
-        vadjustment = self.layout.get_vadjustment()
-        hadjustment = self.layout.get_hadjustment()
-        vscrollbar = Gtk.Scrollbar(orientation=Gtk.Orientation.VERTICAL, adjustment=vadjustment)
-        self.grid.attach(vscrollbar, 1, 0, 1, 1)
-        hscrollbar = Gtk.Scrollbar(orientation=Gtk.Orientation.HORIZONTAL, adjustment=hadjustment)
-        self.grid.attach(hscrollbar, 0, 1, 1, 1)
-
+        # Add the stuff together
+        self.hb.pack_end(self.button)
+        self.scroll.add(self.webview)
+        self.add(self.scroll)
+        # Generate and display the graph
         self.vis = visualizer.Visualizer()
         self.vis.createGraph(self.dirname, self.graph_name) 
-    def on_button_clicked(self, widget):
         self.webview.open("file://localhost" + path.abspath(self.dirname) + "/" + self.graph_name+ ".svg")
+
+    # TODO help window
+    def on_button_clicked(self, widget):
+        return
 
     def show(self):
         self.connect("delete-event", Gtk.main_quit)
         self.show_all()
         Gtk.main()
-
 
 def usage():
     print("How to use:")
@@ -61,9 +62,6 @@ def usage():
     print("  Creates file called graph.svg")
     pritn("Running without any arguments creates graph with default name in default dir.")
 
-# create and run the application, exit with the value returned by
-# running the program
-# TODO Make the program react to the options
 def main():
     is_gui = False
     dirname = "generated_graphs" 
